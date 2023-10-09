8b3d742ec38842920ad03d85baac144345610cbd
pai-puella <cat.dyshkant@yandex.ru>
2023-10-09 14:31:13 +0300

    Added Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        System.out.println();
+    }
 }
